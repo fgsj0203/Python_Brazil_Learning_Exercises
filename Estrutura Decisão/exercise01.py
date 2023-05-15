5b124b2fbf5b213190b761433d3a18c1b114d051
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-15 09:23:58 -0300

    Learning in page Python Brazil with structure decision

---
+++ Estrutura Decisão/exercise01.py
@@ -0,0 +1,15 @@
+"""
+Create two numbers and printing bigger number 
+"""
+
+#First number
+numberOne = int(input("First number: "))
+
+#Second Number
+secondNumber = int(input("Second number: "))
+
+#Condition operation
+if (numberOne > secondNumber):
+    print("Number one is bigger of number two")
+else:
+    print("Number two is bigger of number one")
---
+++ Estrutura Decisão/exercise02.py
@@ -0,0 +1,11 @@
+"""
+Code of printing if value is positive or negative
+"""
+
+#Variable of received number
+number = int(input("Enter with number: "))
+
+if (number >= 0):
+    print("Number is positive")
+else:
+    print("Number is negative")
