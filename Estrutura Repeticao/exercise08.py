5b5dbd7b2389a8947d253798fa07dedd61420f2d
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-17 16:16:08 -0300

    Printing numbers odd in interval

---
+++ Estrutura Repeticao/exercise08.py
@@ -0,0 +1,12 @@
+"""
+Calculate numbers in interval 0 and 50
+"""
+
+
+#Operation of identify numbers odd in interval
+for x in range(0,50):
+    if(x % 2 == 1):
+        print(x)
+    else:
+        pass
+
