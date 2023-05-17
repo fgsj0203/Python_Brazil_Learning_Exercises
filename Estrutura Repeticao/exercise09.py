6c51cdded1267ed90a7658c9c938c6de5285245d
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-17 16:33:04 -0300

    Printing numbers in interval input user

---
+++ Estrutura Repeticao/exercise09.py
@@ -0,0 +1,11 @@
+"""
+Printing numbers in interval of two numbers
+"""
+
+#Variables of numbers
+numberOne = int(input("Enter first number: "))
+numberTwo = int(input("Enter second number: "))
+
+#Operation of printing numbers in interval two numbers
+for x in range(numberOne, numberTwo+1):
+    print(x)
