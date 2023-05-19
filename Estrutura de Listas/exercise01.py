104d5ff7a8b6d660bb34e6ea9414d27e903adb0d
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-18 23:11:17 -0300

    List of five numbers

---
+++ Estrutura de Listas/exercise01.py
@@ -0,0 +1,16 @@
+"""
+Create list of numbers in list and printing
+"""
+
+#Creating list
+listNumbers = []
+
+#Calculating and operation of addition numbers in list
+for x in range(0,5):
+    number = int(input("Enter with number: "))
+    listNumbers.append(number)
+
+
+#Printing list final of numbers
+print("List final of numbers: ",listNumbers)
+
