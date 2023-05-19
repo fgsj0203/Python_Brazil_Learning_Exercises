23e15278c8ee08af524b30e9a5bd712c83975af1
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-18 23:53:40 -0300

    Creating list of numbers and identify numbers pair and odd

---
+++ Estrutura de Listas/exercise05.py
@@ -0,0 +1,27 @@
+"""
+Create list with 20 numbers and calculate numbers pair and odd
+"""
+
+#Creating list empty
+listNumbersGeneral = []
+
+#Creating list of numbers pair
+listNumbersPair = []
+
+#Creating list of numbers odd
+listNumbersOdd = []
+
+#Operation of calculating numbers pair and odd
+for x in range(0,6):
+    number = int(input("Enter with a number: "))
+    listNumbersGeneral.append(number)
+    if (number % 2 == 0):
+        listNumbersPair.append(number)
+    else:
+        listNumbersOdd.append(number)
+
+
+#Printing list of values
+print("List original is: ",listNumbersGeneral)
+print("List of numbers pair: ",listNumbersPair)
+print("List of numbers odd: ",listNumbersOdd)
