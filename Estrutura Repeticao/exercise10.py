5ead61ac256ae782cf189aad1703ea508566d704
Francisco Gomes <fgsj.dev@outlook.com>
2023-05-18 22:40:21 -0300

    Exercise 10 of page Python brazil

---
+++ Estrutura Repeticao/exercise10.py
@@ -0,0 +1,10 @@
+"""
+Create multiplication table 
+"""
+
+#Variable of received number table multiplication
+number = int(input("Enter with number: "))
+
+#Operation of calculate number multiplication table
+for x in range(0,11):
+    print(x * number)
